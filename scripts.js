b969929423450f73b9d9afd0e1cde565b7bf4c2b
Elizabeth Meyer <elizabeth.meyer001@student.swic.edu>
2020-11-05 18:13:45 -0600

    Streamline and clean up.

--- scripts.js
+++ scripts.js
@@ -1,43 +1,11 @@
-const books = [];
-
-const form = document.querySelector("form");
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const genre = document.querySelector("#genre");
-const pgCount = document.querySelector("#pgCount");
-
-title.addEventListener("blur", () => {
-  console.log("babies");
-});
-
-form.addEventListener("submit", function () {
+//new
+document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const formData = {};
-
-  for (let i = 0; i < 4; i++) {
-    formData[event.target.elements[i].id] = event.target.elements[i].value;
-  }
-
-  var book = {
-    title: formData[event.target.elements[0].id],
-    author: formData[event.target.elements[1].id],
-    genre: formData[event.target.elements[2].id],
-    pgCount: formData[event.target.elements[3].id],
-  };
-
-  books.push(book);
-  console.log("Bookshelf:");
-  for (let i = 0; i < books.length; i++) {
-    console.log(books[i]);
-  }
-
-  //Map and Filter Methods
-  const bookTitles = books.map((book) => book.title);
-  console.log(bookTitles);
-
-  const aBooks = books.filter(function (book) {
-    return book.startsWith("A");
-  });
-  console.log(aBooks);
+  const books = Array.from(event.target.elements)
+    .filter(({ id }) => id)
+    .reduce(
+      (accumulator, { id, value }) => ({ ...accumulator, ...{ [id]: value } }),
+      {}
+    );
+  console.log(books);
 });
