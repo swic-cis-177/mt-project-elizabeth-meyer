c9a30c3f2bc4237fc75a71de850d1ee2f23d0152
Elizabeth Meyer <elizabeth.meyer001@student.swic.edu>
2020-12-06 20:08:50 -0600

    Add createTable from student test scores project and integrate into this project.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-//new
+import data from "./data.js";
+
+const tbody = document.querySelector("tbody");
+const template = document.querySelector("template");
+const table = document.querySelector("table");
+
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
   const books = Array.from(event.target.elements)
@@ -8,4 +13,24 @@ document.querySelector("form").addEventListener("submit", (event) => {
       {}
     );
   console.log(books);
+  data.push(books);
+  console.log(data);
+  createBookTable(data);
+
 });
+
+const createBookTable = (data) => {
+  /* while (table.rows.length > 0){
+      table.deleteRow(0);
+  } */
+
+  data.forEach(({ title, author, genre, pgCount }) => {
+    const newBookRow = template.content.cloneNode(true);
+    const newBookTDs = newBookRow.querySelectorAll("td");
+
+    newBookTDs[0].textContent = title;
+    newBookTDs[1].textContent = author;
+    newBookTDs[2].textContent = genre;
+    newBookTDs[3].textContent = pgCount;
+    tbody.appendChild(newBookRow);
+  })};
