f4030497b1e403683e9e038825af2de05c60294e
Elizabeth Meyer <elizabeth.meyer001@student.swic.edu>
2020-12-06 22:25:34 -0600

    Remove repeating data on form reload.

--- scripts.js
+++ scripts.js
@@ -3,27 +3,9 @@ import data from "./data.js";
 const tbody = document.querySelector("tbody");
 const template = document.querySelector("template");
 const table = document.querySelector("table");
-
-document.querySelector("form").addEventListener("submit", (event) => {
-  event.preventDefault();
-  const books = Array.from(event.target.elements)
-    .filter(({ id }) => id)
-    .reduce(
-      (accumulator, { id, value }) => ({ ...accumulator, ...{ [id]: value } }),
-      {}
-    );
-  console.log(books);
-  data.push(books);
-  console.log(data);
-  createBookTable(data);
-
-});
+const tr = document.querySelector("tr");
 
 const createBookTable = (data) => {
-  /* while (table.rows.length > 0){
-      table.deleteRow(0);
-  } */
-
   data.forEach(({ title, author, genre, pgCount }) => {
     const newBookRow = template.content.cloneNode(true);
     const newBookTDs = newBookRow.querySelectorAll("td");
@@ -33,4 +15,20 @@ const createBookTable = (data) => {
     newBookTDs[2].textContent = genre;
     newBookTDs[3].textContent = pgCount;
     tbody.appendChild(newBookRow);
-  })};
+  });
+};
+
+createBookTable(data);
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const books = Array.from(event.target.elements)
+    .filter(({ id }) => id)
+    .reduce(
+      (accumulator, { id, value }) => ({ ...accumulator, ...{ [id]: value } }),
+      {}
+    );
+  data.push(books);
+  tbody.textContent = "";
+  createBookTable(data);
+});
