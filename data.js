a4c949d453e18583a76eb6426ce5e2d22c73ffd7
Elizabeth Meyer <elizabeth.meyer001@student.swic.edu>
2020-12-07 20:49:17 -0600

    Add sample data.

--- data.js
+++ data.js
@@ -1,3 +1,32 @@
 export default [
-  { title: "aaa", author: "aaa", genre: "aaaa", pgCount: "aa" }
+  {
+    title: "The Best Friend",
+    author: "R. L. Stine",
+    genre: "Horror",
+    pgCount: "178",
+  },
+  {
+    title: "Return of the King",
+    author: "J. R. R. Tolkien",
+    genre: "Fantasy",
+    pgCount: "341",
+  },
+  {
+    title: "Dissolving Classroom",
+    author: "Junji Ito",
+    genre: "Horror",
+    pgCount: "102",
+  },
+  {
+    title: "The Turn of the Screw",
+    author: "Henry James",
+    genre: "Horror",
+    pgCount: "341",
+  },
+  {
+    title: "No Exit",
+    author: "Jean-Paul Sartre",
+    genre: "Drama",
+    pgCount: "90",
+  },
 ];
